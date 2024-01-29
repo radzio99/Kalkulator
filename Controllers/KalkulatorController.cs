1a930b8ed174c1abe8791b28536798bca5817dca
radzio99 <radolsaw.rudy@microsoft.wsei.edu.pl>
2024-01-29 23:06:10 +0100

    Dodaj pliki projektów.

---
+++ Controllers/KalkulatorController.cs
@@ -0,0 +1,32 @@
+﻿using Kalkulator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kalkulator.Controllers
+{
+    public class KalkulatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Wynik(KalkulatorModel kalkulator)
+        {
+            int wynik = 0;
+            if (kalkulator.znak == "dod") wynik = kalkulator.Liczba1 +
+           kalkulator.Liczba2;
+            else if (kalkulator.znak == "ode") wynik = kalkulator.Liczba1 -
+           kalkulator.Liczba2;
+            else if (kalkulator.znak == "mnoz") wynik = kalkulator.Liczba1 * kalkulator.Liczba2;
+            else if (kalkulator.znak == "dziel") wynik = kalkulator.Liczba1 / kalkulator.Liczba2;
+            ViewBag.wynik = wynik;
+            return View("Wynik", wynik);
+        }
+        public IActionResult KalkulatorForm()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/KalkulatorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Kalkulator.Models
+{
+    public class KalkulatorModel
+    {
+        public int Liczba1 { get; set; }
+        public int Liczba2 { get;set; }
+        public string znak {  get; set; }
+    }
+}
